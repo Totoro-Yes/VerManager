f63ee7ebf015763e2260fa72315a539fc2a3aa4c
Tootooroo <aydenlin@foxmail.com>
2021-03-18 12:56:25 +0800

    Add misc to worker.

---
+++ manager/worker/misc/jobProcUnitMisc.py
@@ -0,0 +1,35 @@
+# MIT License
+#
+# Copyright (c) 2020 Gcom
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+import asyncio
+from manager.basic.letter import TaskLogLetter
+
+
+async def jobProcUnit_output_proc(datas: bytes, *args) -> None:
+    """
+    Warp log data within TaskLogLetter then send via endpoint.
+    args: [taskid, endpoint]
+    """
+    taskid, endpoint = args[0], args[1]  # type: str, asyncio.DatagramTransport
+    letter = TaskLogLetter(taskid, datas.decode())
+
+    endpoint.sendto(letter.toBytesWithLength())
